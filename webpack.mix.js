02925372438c90cdb6fd6b5703a746320d95f9c5
Marcelo Bianco <marcelognn@gmail.com>
2022-06-13 21:28:38 -0300

    Criando Model Migration e Factory Admin

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+ mix.copyDirectory('resources/_backend', 'public/backend');
+ mix.copyDirectory('resources/_frontend', 'public/frontend');
